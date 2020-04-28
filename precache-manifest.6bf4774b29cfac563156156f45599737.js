c188865fffcb3b3dbb04675292fc65707cb71d15
unknown <enesbc46@gmail.com>
2020-04-28 15:42:19 +0300

    Updates

---
+++ precache-manifest.6bf4774b29cfac563156156f45599737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cce2e912a983c63f32bf39ca7942f40",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4cb320309ead75937e36",
+    "url": "/monsters-rolodex/static/css/main.e889fcad.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb320309ead75937e36",
+    "url": "/monsters-rolodex/static/js/main.0da9e164.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
